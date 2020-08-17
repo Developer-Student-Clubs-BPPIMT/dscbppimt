d352864d6a527dc99f3e7a4037016a36caab4060
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-08-17 16:46:35 +0530

    test components

---
+++ components/Layout.jsx
@@ -0,0 +1,28 @@
+import NavBar from './NavBar';
+import React, { Component } from 'react';
+
+import Head from 'next/head';
+
+class Layout extends Component{
+
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div>
+                <Head>
+                    <title>DSC BPPIMT</title>
+                    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+
+                </Head>
+                <NavBar />
+
+                { this.props.children }
+            </div>
+        );
+    }
+}
+
+export default Layout;
