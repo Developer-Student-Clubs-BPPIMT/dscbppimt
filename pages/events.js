c43e98a57f3b19b9d9913dceae6278d937e3d75c
unknown <shashank080311@gmail.com>
2020-08-20 15:46:04 +0530

    code cleaned

--- pages/events.js
+++ pages/events.js
@@ -1,50 +1,12 @@
 import React from 'react'
-import Navbar from '../components/navbar'
+import styles from "../styles/eventStyles"
 import EventCard from "../components/eventCard"
 import events from "../components/helper/eventsHelper"
 import { withStyles } from '@material-ui/core'
-import Footer from '../components/footer'
 import Searchbar from '../components/Searchbar'
 import Layout from '../components/layout'
 
 
-const styles = theme => ({
-    box:{
-        display: "flex",
-        flexDirection : "column",
-        alignItems:"center",
-        fontFamily: "Roboto"
-    },
-    pastBox: {
-        display: "flex",
-        justifyContent: "center",
-        height : "50vh",
-        backgroundColor: "#dddddd",
-    },
-    header:{
-        display: "flex",
-        justifyContent: "space-between",
-        [theme.breakpoints.down("sm")]:{
-            marginTop: "0",
-            width: "100%",
-            flexDirection: "column",
-            alignItems: "center"
-        },
-
-    },
-    container :{
-        display : "flex",
-        padding: "20px",
-        flexDirection: "column",
-        width: "80%",
-        justfyContent: "flex-start",
-        [theme.breakpoints.down("sm")]:{
-            width: "100%",
-            alignItems: "center"
-        },
-
-    }
-})
 
 function Events({classes}) {
     return (
