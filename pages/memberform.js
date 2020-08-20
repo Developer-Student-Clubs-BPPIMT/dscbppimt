31247390d39cf272a6b2f1580b9a2c87c4966887
Shubham Jha <shubham.jha2001m@gmail.com>
2020-08-20 17:02:43 +0700

    Setting Up Form & Router

---
+++ pages/memberform.js
@@ -0,0 +1,18 @@
+import { Box } from "@material-ui/core";
+import Layout from '../components/layout';
+import { Component } from "react";
+
+
+class MemberForm extends Component {
+    render() {
+        return (
+            <Layout>
+                <Box>
+                    User Form Here
+                </Box>
+            </Layout>
+            )
+    }
+}
+
+export default MemberForm; 
